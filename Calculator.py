fe65fecefc98ba5eaa154272d565384f6b3340b9
Kunjan <kunjannpokhrel@gmail.com>
2026-09-18 20:23:59 +0545

    Removed Introduction

--- Calculator.py
+++ Calculator.py
@@ -1,27 +1,4 @@
 import math
-import time
-import winsound
-
-# Creates A Typing Effect To My Introduction
-print("\033[96m", end="")
-text ="MINI-CALCULATORRR !!"
-for i in text:
-    print(i, end="")
-    winsound.Beep(800, 30)
-    time.sleep(0.09)
-print("")
-time.sleep(1)
-print("\033[91m", end="")
-text ="Creater: KUNJAN POKHREL"
-for i in text:
-    print(i, end="")
-    winsound.Beep(800, 30)
-    time.sleep(0.09)
-print("\033[0m")
-print("")
-time.sleep(1)
-# Introduction Ends
-
 def asmd():
     total = float(input("Enter starting number: "))
     while True:
@@ -41,7 +18,7 @@ def asmd():
     print(f"Final Total: {total}")
 
 def main():
-    print("--- Calculator ---") 
+    print("\033[96--- Calculator --- \033[0m")
     option=int(input("1.Add/Subtract/Multiply/Divide\n2.Power\n3.log()\n4.Square-Root\n5.ln\n6.sin()\n7.cos()\n8.tan()\nChoose One: ") )
     if option==1:
      asmd()
